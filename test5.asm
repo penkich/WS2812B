9a801db7f620ec732849e5fac97e0df1a3c16257
Penkich <nishijim2001@yahoo.co.jp>
2015-02-07 18:39:00 +0900

    Update test5.asm

--- test5.asm
+++ test5.asm
@@ -2,12 +2,15 @@
 ; Serial LED Tape (WS2818B) test program
 ; for AVR ATmega168,328
 ; clock 20MHz
-; 2015-01-31 UART test by penkich
+; 2015-01-11 UART test by penkich
 ;
 .include "m168def.inc"
 .equ n1 = 8  ;
 .equ n2 = 128 ;n1 * n2 = 1k byte
 .org 0x00 jmp reset
+;---------------------------
+; reset
+;---------------------------
 reset:
     ldi r16,low(ramend)
     out spl,r16
@@ -21,44 +24,47 @@ reset:
     ldi xl,0x00
     ldi r17, n1
     ldi r19, n2
-datain:
     jmp main
+;---------------------------
+; led
+; generate pulse
+; in  r22 
+; out portb
+; use r16, r18, r20, r21
+;---------------------------
 led:
-    push r16
-    in r16,sreg
-    push r16
     ldi r18,9       ;loop counter
 loop:
     out portb,r20   ;1
     dec r18         ;1
     cpi r18,0       ;1
     breq return     ;1/2
-    lsl r22         ;1
-    brcs j1         ;1/2
+    lsl r22         ;1    move 1 bit from top to carry
+    brcs j1         ;1/2  if carry = 1
 j0:
-    out portb,r21   ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;8 nop total
-    out portb,r20   ;1
+    out portb,r21   ;1    portb =1
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;8 nop
+    out portb,r20   ;1    portb =0
 j0_1:
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
-    nop             ;1
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;
+    nop             ;9 nop
     rjmp loop       ;2
 j1:
-    out portb,r21
+    out portb,r21   ;     portb =1
     push r16        ;2
     ldi r16,2       ;1
 j1_1:
@@ -69,17 +75,15 @@ j1_1:
     nop
     nop
     nop
-    out portb,r20
+    out portb,r20   ;1    portb =0
     nop
     rjmp loop
 return:
-    out portb,r20   ;1
-    pop r16         ;2
-    out sreg,r16    ;1
-    pop r16         ;2
+    out portb,r20   ;1    portb =0
     ret             ;4
-iloop:
-    rjmp reset
+;---------------------------
+; delay10us
+;---------------------------
                     ;rcall  ;3
 delay10us:
     push r16        ;2
@@ -98,18 +102,26 @@ loop_10us:
     pop r16         ;2
     nop             ;1
     ret             ;4
+;---------------------------
+; uarts
+;---------------------------
 uarts:
     lds r16, ucsr0a
     sbrs r16, udre0
     rjmp uarts
-    sts udr0, r22
+    sts udr0, r22   ;   data -> r22
     ret
+;---------------------------
+; main
+;
+;
+;---------------------------
 main:
     cli
-    ldi r16, 10
-    sts ubrr0l, r16
-    ldi r16, 0
-    sts ubrr0h, r16
+    ldi r16, 10     ;   10 --- 115200kbps, 129 --- 9600bps
+    sts ubrr0l, r16 ;
+    ldi r16, 0      ;
+    sts ubrr0h, r16 ;   115200kbps on 20MHz
     lds r16, ucsr0c
     sbr r16, ((1<<ucsz01)+(1<<ucsz00))
     sts ucsr0c, r16
@@ -117,69 +129,71 @@ main:
     sbr r16, ((1<<rxen0)+(1<<txen0))
     sts ucsr0b, r16
     sei
+;---------------------------
     ldi r17, n1
 loop3:
     ldi r19, n2
-main01:
 loop4:
     lds r16, ucsr0a
     sbrs r16, rxc0
-    rjmp main01
-    lds r22, udr0
-
-    mov r16, r22
+    rjmp loop4
+    lds r22, udr0 ;   received data -> r22
+    mov r16, r22  ;   r22 -> r16
 	push r17
-	rcall chaconv
+	rcall chaconv ;   
     pop r17
-
-    st x+, r16
-    cpi r16, 0x0d
-    breq loop_out 
+    st x+, r16    ;   store data into memory
+    cpi r16, 0x0d ;
+    breq loop_out ;   if data = 'CR'
     rcall uarts
     dec r19
     cpi r19,0
     brne loop4
     dec r17
     brne loop3
-    ldi xh,0x01 ;x <- 0x0100
-    ldi xl,0x00
+;---------------------------
 loop_out:
-	ldi r23, 0; flag
+    ldi xh,0x01 ;   x <- 0x0100
+    ldi xl,0x00
+	ldi r23, 0  ;   flag
     ldi r17, n1
 loop1:
     ldi r19, n2
 loop2:
-    ld r22,x+
+    ld r22,x+   ;   read 1 byte from memory
     cpi r22, 0x0d
-    breq out_led
-;   ldi r16,0b00001111
-;   and r22,r16
+    breq out_led;   if data = 'CR'
 	cpi r23,1
-	brne f0
+	brne f0     ;   if flag not set
 	lsl r24
 	lsl r24
 	lsl r24
-	lsl r24
-	add r22,r24	
-    rcall led
+	lsl r24     ;
+	add r22,r24	;   r22 + r24*16 -> r22
+    rcall led   ;   data send to LED 
 	ldi r23,0
 	rjmp f1
 f0:
-	ldi r23,1
+	ldi r23,1   ;   set flag
 	mov r24,r22
 f1:
     cpi r19,0
     brne loop2
     dec r17
     brne loop1
+;---------------------------
+; stop data and wait
+;---------------------------
 out_led:
     rcall delay10us
 	rcall delay10us
 	rcall delay10us
     jmp reset
-
-;   r16('0','1',...'A','B',...'F')
-;   -> r16(0,1,,,,10,11,...15)
+;---------------------------
+; chaconv
+;   in:  r16('0','1',...'A','B',...'F')
+;   ret: r16(0,1,,,,10,11,...15)
+;---------------------------
 chaconv:
     push r16
     andi r16, 0b00001111
